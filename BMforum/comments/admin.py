095ff8078c9c6d2d12df29c552351fe868689087
ztx <1213587739@qq.com>
2020-05-08 14:24:45 +0800

    qingkong

--- BMforum/comments/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from comments.models import Comment
-# Register your models here.
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('content_object','text','created_time','user')
