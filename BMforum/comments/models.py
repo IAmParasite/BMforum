ed43c6e532f9973a59cee7c401da83df3d7a19aa
IAmParasite <1004183196@qq.com>
2020-05-13 16:24:13 +0800

    v2.5
    实现当前用户发表评论功能

---
+++ BMforum/comments/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+from users.models import User
+ 
+class Comment(models.Model):
+    name = models.CharField('名字', max_length=50)
+    #name = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField('内容')
+    created_time = models.DateTimeField('创建时间', default=timezone.now)
+    post = models.ForeignKey('forum.Post', verbose_name='文章', on_delete=models.CASCADE)
+ 
+    class Meta:
+        verbose_name = '评论'
+        verbose_name_plural = verbose_name
+ 
+    def __str__(self):
+        return '{}: {}'.format(self.name, self.text[:20])
