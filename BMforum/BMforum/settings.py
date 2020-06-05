ee37978d4fec64b4a4839a8089fc2cf9ce9a4830
IAmParasite <1004183196@qq.com>
2020-06-05 14:18:11 +0800

    Release v1.1

--- BMforum/BMforum/settings.py
+++ BMforum/BMforum/settings.py
@@ -139,6 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# 加入下面的配置
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 #其他设置
 AUTH_USER_MODEL = 'users.User'
