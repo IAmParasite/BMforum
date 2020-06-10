253176ea2f07eeaf791c4710c64f485f88e19086
Yinghao Zhu <zhuyinghao1999@msn.cn>
2020-06-10 14:38:10 +0800

    Revert "610"
    This reverts commit a6cb08f9fff3e0b3511ab1985d0e64e956e6258b.

--- BMforum/forum/models.py
+++ BMforum/forum/models.py
@@ -60,7 +60,7 @@ class MemberShip(models.Model):
 
 class GroupPost(models.Model):
    title = models.CharField('标题', max_length=70)
-   body = models.TextField(validators=[MinLengthValidator(25)])
+   body = models.TextField()
    created_time = models.DateTimeField('创建时间', default=timezone.now)
    modified_time = models.DateTimeField('修改时间')
    excerpt = models.CharField(max_length=200, blank=True)
